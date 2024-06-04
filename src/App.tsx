5f620050573010bb41a9b77d05187e8e96dab41f
KagMin <phankangmin@gmail.com>
2024-06-04 22:32:37 +0700

    update src

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/HomePage';
 import MainLayout from './components/Layout';
 import './index.css';
+import HomePage from './pages/HomePage';
 
 const App: React.FC = () => {
   return (
