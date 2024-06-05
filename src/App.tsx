462cd475324db1e441248f5c3a0fc7224ffea2e3
TAT610 <trananhtuyet0610@gmail.com>
2024-06-05 14:48:43 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import './App.css';
 
 
-import { Route, Routes } from "react-router-dom";
-import Contact from './pages/Contact'
-import ShoppingCart from './pages/ShoppingCart';
-import BuyNow from './pages/BuyNow';
-import { Link } from 'react-router-dom'
 function App() {
   return (
     <div className="w-full">
-      {/* <ShoppingCart/> */}
-      <Link to='/shopping' className='mr-6'>Shopping Card</Link>
-      <Link to='/buynow'  className='mr-6'>Buy now</Link>
-      <Link to='/contact'  className='mr-6'>Contact</Link>
-      <Routes>
-      <Route path='/shopping' element={<ShoppingCart/>}/>
-        <Route path='/buynow' element={<BuyNow/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-
-        /</Routes>
+   
 
     </div>
   );
