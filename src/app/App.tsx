cd6e65e491c329bb9c979650217bb7d3f5a36166
KagMin <phankangmin@gmail.com>
2024-06-05 16:56:37 +0700

    Update Cart-Contact-Checkout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import MainLayout from '../components/Layout';
+import BuyNow from '../pages/BuyNow';
 import CategoryPage from '../pages/CategoryPage';
+import Contact from '../pages/Contact';
 import CoursePage from '../pages/CoursePage';
 import HomePage from '../pages/HomePage';
-import Report from '../pages/ReportPage';
+import ReportPage from '../pages/ReportPage';
 import SearchPage from '../pages/SearchPage';
+import ShoppingCard from '../pages/ShoppingCart';
+
 
 
 const App: React.FC = () => (
@@ -15,8 +19,11 @@ const App: React.FC = () => (
         <Route path="/" element={<HomePage/>} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/category" element={<CategoryPage />} />
-        <Route path="/report" element={<Report />} />
-        <Route path="/course" element={<CoursePage />} />
+        <Route path="/course" element={<CoursePage/>} />
+        <Route path="/shoppingCard" element={<ShoppingCard />} />
+        <Route path="/buyNow" element={<BuyNow />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/report" element={<ReportPage />} />
 
       </Routes>
     </MainLayout>
