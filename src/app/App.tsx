624b59e8ed95d34dd1cf6b2b86fd0c81a8fa55a8
KagMin <phankangmin@gmail.com>
2024-06-05 16:07:56 +0700

    aaaa

---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import MainLayout from '../components/Layout';
+// import CategoryPage from '../pages/CategoryPage';
+// import CoursePage from '../pages/CoursePage';
+// import HomePage from '../pages/HomePage';
+// import Report from '../pages/ReportPage';
+// import SearchPage from '../pages/SearchPage';
+
+
+const App: React.FC = () => (
+  <Router>
+    {/* <MainLayout>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/category" element={<CategoryPage />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/course" element={<CoursePage />} />
+
+      </Routes>
+    </MainLayout> */}
+  </Router>
+);
+
+export default App;
