782b2f37fce164d30ad7d78b6b267c53ade6fed0
LeNhuNgoc-Len <lenhungoc1501203@gmail.com>
2024-06-05 11:00:19 +0700

    up code Report Page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CoursePage from '../pages/CoursePage';
+import ReportPage from '../pages/ReportPage';
+
+
 function App() {
   return (
-    <div className="App">
-    <CoursePage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CoursePage />} />
+        <Route path="/report" element={<ReportPage />} />
+      </Routes>
+    </Router>
   );
 }
 
