000fb78bae091636fe5a7e63b6b3a063b8cfbfbe
LeNhuNgoc-Len <lenhungoc1501203@gmail.com>
2024-06-05 15:02:22 +0700

    update again CoursePage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CoursePage from '../pages/CoursePage';
 import ReportPage from '../pages/ReportPage';
+import CoursePage from 'pages/CousrsePage';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CoursePage />} />
+        <Route path="/" element={<CoursePage/>} />
         <Route path="/report" element={<ReportPage />} />
       </Routes>
     </Router>
