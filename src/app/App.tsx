9cd42a2f7427c3cc044c86dfaa0e0377699de10d
LeNhuNgoc-Len <lenhungoc1501203@gmail.com>
2024-06-06 16:16:01 +0700

    created BlogPage, DeatailBlogPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReportPage from '../pages/ReportPage';
 import CoursePage from 'pages/CousrsePage';
+import BlogPage from 'pages/BlogPage';
+import DetailBlogPage from 'pages/DetailBlogPage';
+
 
 
 function App() {
@@ -9,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<CoursePage/>} />
         <Route path="/report" element={<ReportPage />} />
+        <Route path='/blog' element={<BlogPage/>} />
+        <Route path='/detailBlog' element={<DetailBlogPage/>} />
       </Routes>
     </Router>
   );
