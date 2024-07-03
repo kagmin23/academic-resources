e43063e60e36ee622ba3f9d7431497fd3eea6a73
LeNhuNgoc-Len <lenhungoc1501203@gmail.com>
2024-07-03 10:49:17 +0700

    update form

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,1 +1,14 @@
-export const host_main = "https://api-ojt-hcm24-react06-group04.vercel.app/";
+export const HOST_MAIN = "https://api-ojt-hcm24-react06-group04.vercel.app/";
+
+export const apiRequest = async (url: string, options: RequestInit) => {
+  try {
+    const response = await fetch(`${HOST_MAIN}${url}`, options);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   , error);
+    throw error;
+  }
+};
