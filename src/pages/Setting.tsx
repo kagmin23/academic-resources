5030d12a7c0a2f173f4b811191663e30a1f412be
TAT610 <trananhtuyet0610@gmail.com>
2024-07-22 15:45:34 +0700

    checkout

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -122,13 +122,13 @@ const SettingInstructor: React.FC = () => {
                 <Input />
               </Form.Item>
 
-              <Form.Item
+              {/* <Form.Item
                 name="email"
                 label="Email"
                 rules={[{ required: true, message: 'Please input your Email!' }]}
               >
                 <Input />
-              </Form.Item>
+              </Form.Item> */}
 
             
               <Form.Item
--- src/pages/Student/StudentRouter.tsx
+++ src/pages/Student/StudentRouter.tsx
@@ -22,7 +22,7 @@ import ProtectedRouter from '../../components/roles/ProtectedRouter';
 import Report from '../ReportPage';
 import Certificate from './Certificate';
 import RouterLesson from './RouterLesson';
-
+import Checkout from 'pages/Checkout';
 export default function StudentRouter() {
 
   // const LayoutStudent = React.lazy(() => import("../../components/layout/LayoutStudent"));
@@ -69,6 +69,8 @@ export default function StudentRouter() {
                     <Route path={`payment-successfully`} element={<ProtectedRouter allowedRoles={["student"]}><PaymentSuccess /></ProtectedRouter>} />
                     <Route path={`top-instructor`} element={<ProtectedRouter allowedRoles={["student"]}><TopInstructorPage /></ProtectedRouter>} />
                     <Route path={`certificate-student`} element={<ProtectedRouter allowedRoles={["student"]}><Certificate /></ProtectedRouter>} />
+                    <Route path={`checkout`} element={<ProtectedRouter allowedRoles={["student"]}><Checkout /></ProtectedRouter>} />
+
                     </Route>
                </Routes>
 
