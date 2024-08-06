f77328b6826145febaafd7521b332bcc9545f2f4
KagMin <phankangmin@gmail.com>
2024-08-06 19:59:51 +0700

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -31,18 +31,16 @@ const AppRouter: React.FC = () => (
   <AuthProvider>
     <Router>
       <Routes>
+        
         {/* Layout for Guest */}
         <Route path={`/`} element={<LayoutGuest />}>
           {/* <Route index element={<Navigate to="/" />} /> */}
           <Route path={`/`} element={<HomePage />} />
-          {/* <Route path={`/home/course-details/:courseId`} element={<CourseDetailsPage/>} /> */}
-          {/* <Route path={`/course/course-details/:courseId`} element={<CourseDetailsPage/>} /> */}
           <Route path="/log-in" element={<Login />} />
           <Route path="/course-details/:courseId/instructor-detail/:userId/" element={<InstructorDetail />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path={`search`} element={<SearchPage />} />
-          {/* <Route path={`home/course-details/:courseId/`} element={<CourseDetailsPage />} /> */}
           <Route path="/course-details/:courseId/" element={<CourseDetailsPage />} />
           <Route path="/course/course-details/:courseId/" element={<CourseDetailsPage />} />
           <Route path="/course" element={<CoursePage />} />
@@ -54,47 +52,10 @@ const AppRouter: React.FC = () => (
           <Route path="/sign-up-google" element={<SignUpGoogleInstructor />} />
         </Route>
         
-        {/* Layout for Students */}
-       
+        {/* Layout for Users */}
         <Route path="/student/*"  element={<ProtectedRouter allowedRoles={["student"]}><StudentRouter /></ProtectedRouter>}/>
         <Route path="/instructor/*"  element={<ProtectedRouter allowedRoles={["instructor"]}><InstructorRouter /></ProtectedRouter>}/>
         <Route path="/admin/*"  element={<ProtectedRouter allowedRoles={["admin"]}><AdminPage /></ProtectedRouter>}/>
-
-
-        {/* <Route path={``} element={<ProtectedRouter allowedRoles={[2]}><UserRouter /></ProtectedRouter>} /> */}
-        {/* <Route path={``} element={<HomePage />} />
-          <Route path={`blog`} element={<BlogPage />} />
-          <Route path={`about`} element={<About />} />
-
-          <Route path={`course`}element={<ProtectedRouter allowedRoles={[2]}><CoursePage /></ProtectedRouter>} />
-          <Route path={`category`} element={<ProtectedRouter allowedRoles={[2]}><CategoryPage /></ProtectedRouter>} />
-          <Route path={`shopping-cart`} element={<ProtectedRouter allowedRoles={[2]}><ShoppingCart /></ProtectedRouter>} />
-          <Route path={`profile-student`} element={<ProtectedRouter allowedRoles={[2]}><ProfileStudent /></ProtectedRouter>} />
-          <Route path={`buy-now`} element={<ProtectedRouter allowedRoles={[2]}><BuyNow /></ProtectedRouter>} />
-          <Route path={`contact`} element={<ProtectedRouter allowedRoles={[2]}><Contact /></ProtectedRouter>} />
-          <Route path={`report`} element={<ProtectedRouter allowedRoles={[2]}><Report /></ProtectedRouter>} />
-          <Route path={`setting`} element={<ProtectedRouter allowedRoles={[2]}><Setting /></ProtectedRouter>} />
-          <Route path={`lesson-student`} element={<ProtectedRouter allowedRoles={[2]}><LessonStudent /></ProtectedRouter>} />
-          <Route path={`save`} element={<ProtectedRouter allowedRoles={[2]}><SavePage /></ProtectedRouter>} />
-          <Route path={`payment-successfully`} element={<ProtectedRouter allowedRoles={[2]}><PaymentSuccess /></ProtectedRouter>} /> */}
-        {/* </Route> */}
-          {/* <Route path="/home" element={<HomePage />} /> */}
-
-        {/* <Route path="/admin/*" element={<LayoutAdmin />}> */}
-          {/* <Route path="home" element={<HomePage />} /> */}
-           {/* <Route path={``} element={<ProtectedRouter allowedRoles={[1]}><AdminPage /></ProtectedRouter>} />
-
-          </Route>  */}
-
-          {/* <Route path="/instructor/*" element={<LayoutInstructor />}> */}
-          {/* <Route path="admin-page/*" element={<ProtectedRouter allowedRoles={[1]}><AdminPage /></ProtectedRouter>} />
-          <Route path="profile-instructor/*" element={<ProtectedRouter allowedRoles={[3]}><ProfileInstructor /></ProtectedRouter>} /> */}
-
-          {/* <Route path={`instructor-page`} element={<ProtectedRouter allowedRoles={[3]}><InstructorPage /></ProtectedRouter>} /> */}
-
-          {/* </Route> */}
-          
-
       </Routes>
 
     </Router>
